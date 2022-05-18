ef4cdf3fb22a084f95cf354b9a70cc98a85e12da
AnjiMuduthanapally <105631021+AnjiMuduthanapally@users.noreply.github.com>
2022-05-18 13:38:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
 }
-provider "azuread" {
- 
-   subscription_id = var.subscription_id
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
