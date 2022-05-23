1e74affae17d30c664dbdd2607f031e1bb299943
AnjiMuduthanapally <105631021+AnjiMuduthanapally@users.noreply.github.com>
2022-05-23 13:16:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,16 +19,4 @@ variable "subscription_id" {
  
    type = string
 }
-variable "client_id" {
-    
-    type = string
-}
-variable "client_secret" { 
-
-    type = string
-}
-variable "tenant_id" {
-    type = string
-
-}
 
